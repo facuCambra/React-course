95de828ce320403f72ba9cfc7ebcfc92ece6a68e
facuCambra <facucambra4@outlook.com>
2022-03-30 23:10:20 -0300

    cierre seccion

--- 06 gif app/gif-expert-app/src/Components/GifGrid.js
+++ 06 gif app/gif-expert-app/src/Components/GifGrid.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 import {getGifs} from './helpers/getGifs'
 export const GifGrid = ({category}) => {
     
-    const [images, setImages] = useState([]);
-
-
-    useEffect( ()=>{
-        getGifs(category)
-            .then(setImages); // = a then(images=>set(images))
-    }, [category])    //si la categoria cambia renderiza denuevo
-
-    
-
+    const {data:images ,loading} = useFetchGifs(category);
+ 
     return (
     <>
         <h3>{category}</h3>
+        { loading && <p className='animate__animated animate__flash"'>Loading</p>}
         <div className='card-grid'>
             {
             images.map( img => (
@@ -25,12 +19,10 @@ export const GifGrid = ({category}) => {
                     {...img} //envia cada prop de img de forma independiente
                 />
             ))
-            // images.map( ({id, title}) => (
-            //     ( <li key={id}> {title}</li> )
-            // ))
+          
             }
         
-        </div>
+        </div> 
     </>
   )
 }
