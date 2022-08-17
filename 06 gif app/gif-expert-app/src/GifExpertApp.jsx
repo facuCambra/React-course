4e2e91782a7ef5f2160e3b1e609d457de960bdda
facuCambra <facucambra4@outlook.com>
2022-08-17 14:11:50 -0300

    feat: setup of section 8 ready

---
+++ 06 gif app/gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,38 @@
+import React , {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {AddCategory} from './Components/AddCategory';
+import { GifGrid } from './Components/GifGrid';
+export const GifExpertApp =() =>{
+
+    //const categories = ['Comedia', 'Accion', 'Aventura'];
+    const initialState = ['Rock'];
+    const [categories, setCategories] = useState(initialState);
+    /* 
+    const handleAdd =() =>{
+        setCategories( [ ...categories , 'Drama']);
+        // setCategories( categories=> [ ...categories , 'Drama']);
+    };*/
+    return (
+        <>
+            <h2> GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />  {/*Para comunicar componentes */}
+            <hr />
+            {/* <button onClick={handleAdd}> Agregar </button> */}
+            <ol>
+                {
+                    // categories.map( (categorie, index) =>{
+                    //     return <li key={index}> {categorie}</li>
+                    //})
+                    categories.map( category => (
+                        <GifGrid 
+                            key={category}
+                            category={category} 
+                        />)
+                    )
+                }
+            </ol>
+
+            
+        </>
+    );
+}
